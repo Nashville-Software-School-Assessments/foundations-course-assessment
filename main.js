7334baba65dda8ac9931967ea6fdd834782c7da5
stevebrownlee <stevebrownlee@users.noreply.github.com>
2022-02-26 12:06:55 -0600

    Update main.js

--- main.js
+++ main.js
@@ -4,21 +4,47 @@
 */
 
 /*
-    This function should receive both book objects as input
-    and then add their prices together. It should return
-    the sum of the prices
+    Responsibility:
+        This function should receive both book objects as input
+        and then add their prices together. It should return
+        the sum of the prices.
+        
+    Parameters:
+        1. (object) - A single, distinct book object
+        2. (object) - A single, distinct book object
+        
+    Returns:
+        (number) - The sum of the price of both books
 */
 const addAndReturnSum = () => {
 }
 
+
 /*
-    This function should receive any single book as input,
-    and then return the title of the book
+    Responsibility:
+        Provide just the title of a book
+        
+    Parameters:
+        1. (object) - A single, distinct book object
+        
+    Returns:
+        (string) - The title of the book object parameter
 */
 const accessTitleAndReturn = () => {
 
 }
 
+
+/*
+    Responsibility:
+        Determine if a book should be stock on the Bargains table in the store
+        
+    Parameters:
+        1. (object) - A single, distinct book object
+        
+    Returns:
+        (boolean) - `true` if the price of the book is less than $20, otherwise `false`
+*/
 
 const taiko = { id: 1, title: "Taiko", author: "Eiji Yoshikawa", price: 19.00, yearPublished: 1998 }
 const quicksilver = { id: 2, title: "Quicksilver", author: "Neal Stephenson", price: 34.00, yearPublished: 2003 }
