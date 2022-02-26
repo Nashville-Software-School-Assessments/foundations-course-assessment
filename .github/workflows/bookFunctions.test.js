674d693fa8cd63d567e87376e8cd92c6aa3aecb8
Steve Brownlee <chortlehoort@gmail.com>
2022-02-26 13:40:32 -0600

    trying new command sequence

--- .github/workflows/bookFunctions.test.js
+++ .github/workflows/bookFunctions.test.js
@@ -1,15 +1,61 @@
-const { accessTitleAndReturn, addAndReturnSum } = require("../../main.js")
+import studentCode from "../../src/scripts/main.js"
+
+
+const {
+    accessTitleAndReturn,
+    addAndReturnSum,
+    isBargainBook } = studentCode
 
 
 const taiko = { id: 1, title: "Taiko", author: "Eiji Yoshikawa", price: 19.00, yearPublished: 1998 }
 const quicksilver = { id: 2, title: "Quicksilver", author: "Neal Stephenson", price: 34.00, yearPublished: 2003 }
 
+describe("Price sum", () => {
 
-test('Calculates sum', () => {
-    expect(addAndReturnSum(taiko, quicksilver)).toBe(53);
+    test('addAndReturnSum function is defined', () => {
+        expect(addAndReturnSum).toBeDefined();
+    });
+
+    test('addAndReturnSum function returns something', () => {
+        expect(addAndReturnSum(taiko, quicksilver)).toBeDefined();
+    });
+
+    test('addAndReturnSum calculates sum correctly', () => {
+        expect(addAndReturnSum(taiko, quicksilver)).toBe(53);
+    });
+})
+
+describe("Book title", () => {
+    test('accessTitleAndReturn function is defined', () => {
+        expect(accessTitleAndReturn).toBeDefined();
+    });
+
+    test('accessTitleAndReturn function returns something', () => {
+        expect(accessTitleAndReturn(quicksilver)).toBeDefined();
+    });
+
+    test('accessTitleAndReturn returns title value', () => {
+        const title = accessTitleAndReturn(quicksilver)
+        expect(title).toBe("Quicksilver");
+    });
 });
 
-test('Returns title', () => {
-    const title = accessTitleAndReturn(quicksilver)
-    expect(title).toBe("Quicksilver");
+describe("Bargain books", () => {
+    test('isBargainBook function is defined', () => {
+        expect(isBargainBook).toBeDefined();
+    });
+
+    test('isBargainBook function returns something', () => {
+        expect(isBargainBook(quicksilver)).toBeDefined();
+    });
+
+    test('Checks for non-bargain book', () => {
+        const bargain = isBargainBook(quicksilver)
+        expect(bargain).toBe(false);
+    });
+
+    test('Checks for bargain book', () => {
+        const bargain = isBargainBook(taiko)
+        expect(bargain).toBe(true);
+    });
 });
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,11 +8,11 @@
         This function should receive both book objects as input
         and then add their prices together. It should return
         the sum of the prices.
-        
+
     Parameters:
         1. (object) - A single, distinct book object
         2. (object) - A single, distinct book object
-        
+
     Returns:
         (number) - The sum of the price of both books
 */
@@ -23,10 +23,10 @@ const addAndReturnSum = () => {
 /*
     Responsibility:
         Provide just the title of a book
-        
+
     Parameters:
         1. (object) - A single, distinct book object
-        
+
     Returns:
         (string) - The title of the book object parameter
 */
@@ -38,13 +38,16 @@ const accessTitleAndReturn = () => {
 /*
     Responsibility:
         Determine if a book should be stock on the Bargains table in the store
-        
+
     Parameters:
         1. (object) - A single, distinct book object
-        
+
     Returns:
         (boolean) - `true` if the price of the book is less than $20, otherwise `false`
 */
+const isBargainBook = () => {
+
+}
 
 const taiko = { id: 1, title: "Taiko", author: "Eiji Yoshikawa", price: 19.00, yearPublished: 1998 }
 const quicksilver = { id: 2, title: "Quicksilver", author: "Neal Stephenson", price: 34.00, yearPublished: 2003 }
@@ -54,13 +57,12 @@ const quicksilver = { id: 2, title: "Quicksilver", author: "Neal Stephenson", pr
 // Invoke addAndReturnSum and specify the correct arguments
 const sumOfBooks =
 
-
-// Invoke accessTitleAndReturn and specify the correct argument
-const titleOfBook =
-
-
 // If your function algorithm is correct, this should output "Sum of prices is 54"
 console.log(`Sum of prices is ${sumOfBooks}`)
+
+
+// Invoke accessTitleAndReturn and specify a correct argument value
+const titleOfBook =
 
 /*
     If your function algorithm is correct, this should output one of the following
@@ -69,6 +71,64 @@ console.log(`Sum of prices is ${sumOfBooks}`)
         "Title of book is Quicksilver"
 */
 console.log(`Title of book is ${titleOfBook}`)
+
+
+// Invoke isBargainBook and specify a correct argument value
+const placeOnBargainRack =
+
+
+/*
+    If your function algorithm is correct, this should output either
+        "Should the book be a bargain? true"
+    or
+        "Should the book be a bargain? false"
+*/
+console.log(`Should the book be a bargain? ${placeOnBargainRack}`)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -89,7 +149,5 @@ console.log(`Title of book is ${titleOfBook}`)
 
 
 // **********  Do not touch this code  **********
-module.exports = {
-    accessTitleAndReturn, addAndReturnSum
-}
+export default { accessTitleAndReturn, addAndReturnSum, isBargainBook }
 // **********  Do not touch this code  **********
